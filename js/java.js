b697584effb34d2dc514ef7d9cd21bd2b69ce5b4
HeitorPJ <heitorpolido06@gmail.com>
2026-04-09 00:03:32 -0300

    first commit

---
+++ js/java.js
@@ -0,0 +1,11 @@
+function voltarAoTopo() {
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth'
+            });}
+
+function btnQuemSomos() {
+    document.getElementById("QuemSomos-js").scrollIntoView({
+        behavior: "smooth"
+    });
+}
