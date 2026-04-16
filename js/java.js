a8411d509c2e225dd5550510bccce05fea7bd8c0
HeitorPJ <heitorpolido06@gmail.com>
2026-04-16 00:13:42 -0300

    Add mapa

--- js/java.js
+++ js/java.js
@@ -34,3 +34,31 @@ function IrContato() {
         behavior: "smooth"
     });
 }
+var map = L.map('mapa-imagem', {
+    scrollWheelZoom: false,
+    touchZoom: true,
+
+}).setView([-15.111675, -53], 3.5);
+
+L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+var locais = [
+    { nome: "Vitória - ES", coords: [-20.315357, -40.337874] },
+    { nome: "São Paulo - SP", coords: [-23.5505, -46.6333] },
+    { nome: "BH - MG", coords: [-19.9167, -43.9345] },
+    { nome: "Belem - PA", coords: [-1.449461, -48.492891] },
+    { nome: "Brasilia - DF", coords: [-15.777537, -47.897793] }
+];
+var iconeAzul = L.icon({
+    iconUrl: 'IMG/map-marker.svg',
+    iconSize: [40, 40],
+    iconAnchor: [20, 40]
+});
+
+locais.forEach(local => {
+    L.marker(local.coords, { icon: iconeAzul })
+        .addTo(map)
+        .bindPopup(`<b>${local.nome}</b>`);
+});
