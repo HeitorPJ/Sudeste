ee441e68528bff06258612dd6a85b66c3a0dd32b
HeitorPJ <heitorpolido06@gmail.com>
2026-04-22 00:05:03 -0300

    Add responsividade

--- js/java.js
+++ js/java.js
@@ -1,8 +1,9 @@
+/*-------Troca a logo quando o zoom esta entre 950 e 1050*/
 function trocarLogo() {
     const logo = document.getElementById("btnVoltarAoTopo");
     const header = document.querySelector("header");
 
-    if (window.innerWidth <= 1050 && window.innerWidth >= 1000 ) {
+    if (window.innerWidth <= 1050 && window.innerWidth >= 950 ) {
         logo.src = "IMG/Logo-2.png";
         header.style.padding = "35px 0";
     } else {
@@ -10,16 +11,48 @@ function trocarLogo() {
         header.style.padding = "10px 0";
     }
 }
-
 window.addEventListener("resize", trocarLogo);
 trocarLogo();
 
+/*-------Abrir e fechar menu*/
 
+const menuBtn = document.getElementById("btn-menu");
+const menu = document.getElementById("btnsHeader");
+const icon = document.getElementById("menu-icon");
+const itensMenu = document.querySelectorAll(".nav-item");
+
+menuBtn.addEventListener("click", () => {
+    menu.classList.toggle("active");
+
+    if (menu.classList.contains("active")) {
+        icon.src = "IMG/close.svg";
+    } else {
+        icon.src = "IMG/menu.svg";
+    }
+});
+
+// fechar ao clicar em qualquer item
+itensMenu.forEach(item => {
+    item.addEventListener("click", () => {
+        menu.classList.remove("active");
+        icon.src = "IMG/menu.svg";
+    });
+});
+
+/*------ funcoes do header-------*/
 function voltarAoTopo() {
             window.scrollTo({
                 top: 0,
                 behavior: 'smooth'
-            });}
+            });
+    const menu = document.getElementById("btnsHeader");
+    const icon = document.getElementById("menu-icon");
+
+    menu.classList.remove("active");
+    icon.src = "IMG/menu.svg";
+
+
+}
 function IrClientes() {
     document.getElementById("clientes").scrollIntoView({
         behavior: "smooth"
@@ -52,6 +85,8 @@ function IrContato() {
     });
 }
 
+
+// funcionalidade do carousel
 const track = document.querySelector('.carousel-track');
 const slides = document.querySelectorAll('.carousel-track img');
 
@@ -72,6 +107,7 @@ function updateCarousel() {
 }
 
 
+// funcionalidades do mapa 
 var map = L.map('mapa-imagem', {
     scrollWheelZoom: false,
     touchZoom: true,
@@ -100,3 +136,13 @@ locais.forEach(local => {
         .addTo(map)
         .bindPopup(`<b>${local.nome}</b>`);
 });
+
+// ------- pop-up
+function abrirPopup() {
+    document.getElementById("popup-email").classList.add("ativo");
+}
+
+function fecharPopup() {
+    document.getElementById("popup-email").classList.remove("ativo");
+}
+
